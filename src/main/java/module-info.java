a91aa55a98a46034e1035a90744e0dcb34137c5b
Hoyt Summers Pittman <secondsun@gmail.com>
2023-12-28 12:53:12 -0500

    wip

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 open module dev.secondsun.retro.util {
     requires java.logging;
     requires java.xml;
-    requires dev.secondsun.tm4e.core;
+    requires transitive dev.secondsun.tm4e.core;
 
     exports dev.secondsun.retro.util;
-}
+    }
